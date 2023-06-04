4a0492a1ec69b9a72a42c8536d49360e9f4973fb
Katherine-Sokol <katherine.kortyak@gmail.com>
2023-06-04 23:25:35 +0300

    added constraints to columns

--- src/main/java/com/goit/entity/Client.java
+++ src/main/java/com/goit/entity/Client.java
@@ -1,15 +1,20 @@
 package com.goit.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
+
 
 @Table(name = "client")
 @Entity
 @Data
 public class Client {
+
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
     private int id;
+
     @Column
+    @Size (min = 3, max = 200)
     private String name;
 }
--- src/main/java/com/goit/entity/Planet.java
+++ src/main/java/com/goit/entity/Planet.java
@@ -4,14 +4,21 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
+
 
 @Table(name = "planet")
 @Entity
 @Data
 public class Planet {
+
     @Id
+    @Pattern(regexp = "\\b[A-Z0-9]+\\b")
     private String id;
+
     @Column
+    @Size(min = 1, max = 500)
     private String name;
 }
